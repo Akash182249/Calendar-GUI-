d6a3f5a15013305adc8e4a4a2c240a7539202f90
Akash <87010703+Akash182249@users.noreply.github.com>
2021-10-30 23:17:56 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,12 @@
+a = 96
+b = 69
+
+#Arithmetic Operators
+ 
+print("The value of 96+69 is ",96+69) #Addition Operator
+print("The value of 96-69 is ",96-69) #Substraction Operator
+print("The value of 96*69 is ",96+69) #Multiplication Operator
+print("The value of 96/69 is ",96+69) #Division Operator
+print("The value of 96%69 is ",96%69) #Modulus Operator
+print("The value of 96**69 is ",96**69) #Exponentiation Operator
+print("The value of 96//69 is ",96//69) #Floor Division 
